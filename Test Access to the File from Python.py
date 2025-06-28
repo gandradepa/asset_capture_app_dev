5f415600c2021916438c2aacfad2a0466e0ae693
gandradepa <gilberto.andrade@ubc.ca>
2025-06-28 14:08:25 -0700

    Your commit message

--- Test Access to the File from Python.py
+++
@@ -1,4 +0,0 @@
-import os
-
-path =r"S:\MaintOpsPlan\AssetMgt\Asset Management Process\Database\8. New Assets\QR_code_project\asset_capture_app\data\QR_codes.accdb"
-print("File exists:", os.path.exists(path))
